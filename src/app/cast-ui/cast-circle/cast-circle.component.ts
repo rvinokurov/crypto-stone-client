230bc7c1a525eda0239bea9e1745faa3f52794c3
Roman Vinokurov <r.vinokurov@yclients.tech>
2018-05-09 02:45:01 +0300

    add cast elements

--- src/app/cast-ui/cast-circle/cast-circle.component.ts
+++ src/app/cast-ui/cast-circle/cast-circle.component.ts
@@ -2,28 +2,31 @@ import {AfterViewInit, Component, ElementRef, Input, OnInit, Renderer2, ViewChil
 import {ElementSector} from './element-sector';
 
 
+const countOfType = 5;
+const angleStep = (Math.PI / 2.2 - Math.PI / 21) / (countOfType + 1);
+
 const SectorsConfig = [
   {
-    startAngle: -Math.PI / 2,
-    endAngle: -Math.PI / 15,
+    startAngle: -Math.PI / 2.2,
+    endAngle: -Math.PI / 21,
     color: `#55493D`,
     name: 'earth'
   },
   {
-    startAngle: -Math.PI / 15,
+    startAngle: -Math.PI / 21,
     endAngle: Math.PI / 2 - Math.PI / 7.5,
     color: `#62F5FA`,
     name: 'water'
   },
   {
     startAngle: Math.PI / 2 + Math.PI / 7.5,
-    endAngle: Math.PI + Math.PI / 15,
+    endAngle: Math.PI + Math.PI / 21,
     color: `#B13600`,
     name: 'fire'
   },
   {
-    startAngle: Math.PI + Math.PI / 15,
-    endAngle: Math.PI + Math.PI / 2,
+    startAngle: Math.PI + Math.PI / 21,
+    endAngle: Math.PI + Math.PI / 2.2,
     color: `#AFCDFC`,
     name: 'air'
   }
@@ -31,6 +34,12 @@ const SectorsConfig = [
 
 
 const hoverClass = 'cast-circle--elements-sector_hover';
+
+export interface CastElement {
+  x: string;
+  y: string;
+  type: string;
+}
 
 @Component({
   selector: 'app-cast-circle',
@@ -48,13 +57,18 @@ export class CastCircleComponent implements OnInit, AfterViewInit {
 
   private sectors: ElementSector[] = [];
 
-  constructor(private renderer: Renderer2) {
+  private elements: CastElement[] = [];
+
+  constructor(private renderer: Renderer2, private element: ElementRef) {
   }
 
   ngOnInit() {
   }
 
   ngAfterViewInit() {
+    if (!this.isPlayer) {
+      return;
+    }
     SectorsConfig.forEach((config) => {
       this.sectors.push(new ElementSector({
         element: this[config.name].nativeElement,
@@ -63,10 +77,35 @@ export class CastCircleComponent implements OnInit, AfterViewInit {
         color: config.color
       }));
     });
+
+    const r = this.element.nativeElement.getBoundingClientRect();
+    const width = r.width;
+    const height = r.height;
+    const cx = width / 2;
+    const cy = height / 2;
+    const radius = ((width > height ? height : width) / 2) / 1.2;
+
+
+    SectorsConfig.forEach((sector) => {
+      const angle = sector.startAngle + angleStep;
+
+      for (let i = 0; i < countOfType; i++) {
+        const currentAngle = angle + i * angleStep;
+        console.log('a', currentAngle);
+        const x = cx + radius * Math.cos(currentAngle) ;
+        const y = cy + radius * Math.sin(currentAngle);
+
+        this.elements.push({
+          type: sector.name,
+          x: `${x}px`,
+          y: `${y}px`
+        });
+      }
+    });
+
   }
 
   mousemove(event: MouseEvent) {
-    console.log(event);
     this.sectors.forEach((sector) => {
       if (sector.inSector(event)) {
         this.renderer.addClass(sector.canvas, hoverClass);
--- src/app/cast-ui/cast-element/cast-element.component.ts
+++ src/app/cast-ui/cast-element/cast-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostBinding, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-cast-element',
@@ -7,7 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CastElementComponent implements OnInit {
 
-  constructor() { }
+  @Input() elementType;
+
+  constructor() {
+  }
+
+  @HostBinding('class') get classByType() {
+    return this.elementType;
+  }
 
   ngOnInit() {
   }
